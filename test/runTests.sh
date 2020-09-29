6d9c38e1d39a4438527365629bb76345744fa11d
abrighto <Allan.Brighton@t-online.de>
2020-09-29 15:25:13 +0200

    Updated for latest csw release (3.0.0-M1)

--- test/runTests.sh
+++ test/runTests.sh
@@ -9,8 +9,8 @@ if ! hash csw-services.sh 2>/dev/null ; then
     exit 1
 fi
 set -v
-csw-services.sh --version v3.0.0-M1 start -e > $logfile 2>&1 &
-#csw-services.sh start -e > $logfile 2>&1 &
+#csw-services.sh --version v3.0.0-M1 start -e > $logfile 2>&1 &
+csw-services.sh start -e > $logfile 2>&1 &
 cd testSupport || exit 1
 sbt stage  >> $logfile 2>&1
 test-deploy/target/universal/stage/bin/test-container-cmd-app --local test-deploy/src/main/resources/TestContainer.conf >> $logfile 2>&1 &
