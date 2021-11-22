004f85ac6aaba9df5d852623b208885d8cd3f25f
Allan Brighton <Allan.Brighton@t-online.de>
2021-11-22 11:34:00 +0100

    Fixed install.sh issue with libzlog

--- install.sh
+++ install.sh
@@ -7,18 +7,23 @@
 dir=install/csw-c
 rm -rf $dir
 
+# Note that zlog is from source on Linux, brew on Mac
+SYS_LIBS="hiredis cbor uuid"
+LOCAL_LIBS=""
+
 os="$(uname -s)"
 case "${os}" in
     Linux*)
       LIB_SUFFIX=so
+      LOCAL_LIBS="$LOCAL_LIBS zlog"
       SYS_LIB_DIR=/lib/x86_64-linux-gnu;;
     Darwin*)
       LIB_SUFFIX=dylib
+      SYS_LIBS="$SYS_LIBS zlog"
       SYS_LIB_DIR=/usr/local/lib;;
     *)
       echo "Unsupported os: $os"
 esac
-SYS_LIBS="hiredis cbor zlog uuid"
 TARGET_LIB_DIR=$dir/lib/$os
 
 set -x
@@ -29,6 +34,12 @@ cp csw/csw.h $dir/include
 # Native C dependencies (copy with links)
 for i in $SYS_LIBS; do
   cp $SYS_LIB_DIR/lib$i.$LIB_SUFFIX* $TARGET_LIB_DIR
+done
+for i in $LOCAL_LIBS; do
+  (cd $LOCAL_LIB_DIR; tar cf - lib$i.$LIB_SUFFIX*) | (cd $TARGET_LIB_DIR; tar xf -)
+  if test "$os" = "Darwin" ; then
+    (cd $LOCAL_LIB_DIR; tar cf - lib$i.*.$LIB_SUFFIX*) | (cd $TARGET_LIB_DIR; tar xf -)
+  fi
 done
 
 make
